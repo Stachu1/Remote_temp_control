4fbad5d28d762881b9b3a9a01743621e620fdcd9
Stachu1 <stach.basiuk@gmail.com>
2023-02-03 21:55:36 +0100

    Client done

---
+++ client/client.ino
@@ -0,0 +1,138 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Adafruit_BMP280.h>
+#include <Wire.h>
+#include <EEPROM.h>
+
+#define HEATER 5
+#define LED 2
+#define BUTTON 12
+#define ADDR 0
+#define TEMP_CHECK_INTERVAL 3000
+#define TEMP_SEND_INTERVAL 300000
+
+
+const char* SSID = "SSID";
+const char* PASSWORD = "PASSWORD";
+const char* URL = "http://YOUR_IP/temp/";
+
+float temp;
+uint8_t temp_target;
+uint64_t temp_check_timer = 0;
+uint64_t temp_send_timer = 0;
+
+Adafruit_BMP280 bmp;
+HTTPClient http;
+
+
+void setup() {
+  pinMode(HEATER, OUTPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT_PULLUP);
+  
+  Serial.begin(115200);
+  EEPROM.begin(ADDR + 1);
+  temp_target = EEPROM.read(ADDR);
+
+  if (!bmp.begin(0x76)) {
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+    while (true);
+  }
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,Adafruit_BMP280::SAMPLING_X2,Adafruit_BMP280::SAMPLING_X16,Adafruit_BMP280::FILTER_X16,Adafruit_BMP280::STANDBY_MS_500);
+
+  connect();
+  network_update();
+  temp_update();
+}
+
+void loop() {
+  if (millis() - temp_send_timer >= TEMP_SEND_INTERVAL || digitalRead(BUTTON) == LOW) {
+    delay(50);
+    while(digitalRead(BUTTON) == LOW) {
+      digitalWrite(LED, HIGH);
+    }
+    digitalWrite(LED, LOW);
+    temp_send_timer = millis();
+    network_update();
+  }
+
+  if (millis() - temp_check_timer >= TEMP_CHECK_INTERVAL) {
+    temp_check_timer = millis();
+    temp_update();
+  }
+}
+
+
+void temp_update() {
+  digitalWrite(LED, HIGH);
+  delay(100);
+  digitalWrite(LED, LOW);
+
+  temp = bmp.readTemperature();
+
+  Serial.print("\nTemp: ");
+  Serial.print(temp);
+  Serial.println(" °C");
+  Serial.print("Temp target: ");
+  Serial.print(temp_target);
+  Serial.println(" °C\n");
+  
+  if (temp_target > 25) {
+    temp_target = 25;
+  }
+  if (temp < temp_target) {
+    digitalWrite(HEATER, HIGH);
+  }
+  else if (temp > temp_target + 1) {
+    digitalWrite(HEATER, LOW);
+  }  
+}
+
+
+void network_update() {
+  if(WiFi.status()== WL_CONNECTED) {
+    http.begin(URL + String(temp));
+    
+    int response_code = http.GET();
+
+    Serial.print("HTTP Response code: ");
+    Serial.println(response_code);
+    if (response_code > 0) {
+      String data = http.getString();
+      temp_target = data.toInt();
+      if (EEPROM.read(ADDR) != temp_target) {
+        EEPROM.write(ADDR, temp_target);
+        EEPROM.commit();
+      }
+    }
+    http.end();
+  }
+  else {
+    Serial.println("WiFi Disconnected");
+    connect();
+  }
+}
+
+
+void connect() {
+  Serial.print("\nConnecting to ");
+  Serial.println(SSID);
+  WiFi.begin(SSID, PASSWORD);
+  uint8_t counter = 0;  
+  while (WiFi.status() != WL_CONNECTED && counter < 50) {
+    digitalWrite(LED, HIGH);
+    delay(100);
+    digitalWrite(LED, LOW);
+    delay(100);
+    counter++;
+  }
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Connected!");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+    Serial.println("\n");
+  }
+  else {
+    Serial.println("Connection couldn't be established");
+  }
+}
